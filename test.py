3338e82ebeb8ba193fbc4a7167087a4e4e5c111c
Manideep1308 <manideep.sankabathula@innoart.in>
2022-06-21 10:05:46 +0530

    added subprocess -run

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ subscription = "6312cb34-7d60-4d8a-b56a-3f952aa74df6"
 
 def azure_vnet_list():
     for resource_list in ResourceManagementClient(credential, subscription).resources.list():
-        if resource_list.type == "Microsoft.Network/networkSecurityGroups":
+        if resource_list.type == "Microsoft.Network/virtualNetworks":
             print(f"{resource_list.name}")
 
 def main():
